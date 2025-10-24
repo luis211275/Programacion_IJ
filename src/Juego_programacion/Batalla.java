0955b521fff557a03e436c6c6d3f07f773ec6ec0
luis211275 <luislopeznunosanchez@gmail.com>
2025-10-24 12:14:24 +0200

    commit

--- src/Juego_programacion/Batalla.java
+++ src/Juego_programacion/Batalla.java
@@ -1,25 +1,97 @@
 package Juego_programacion;
 
 public class Batalla {
-    private int vida_arquero;
-    private int ataque_arquero;
-    private int defensa_arquero;
-    private int vida_guerrero;
-    private int ataque_guerrero;
-    private int defensa_guerrero;
-    private int vida_mago;
-    private int ataque_mago;
-    private int defensa_mago;
+    int vida_guerrero;
+    int ataque_guerrero;
+    int defensa_guerrero;
+    int vida_mago;
+    int ataque_mago;
+    int defensa_mago;
+    int vida_arquero;
+    int ataque_arquero;
+    int defensa_arquero;
 
-    public Batalla(int vida_arquero, int ataque_arquero, int defensa_arquero, int vida_guerrero, int ataque_guerrero, int defensa_guerrero, int vida_mago, int ataque_mago, int defensa_mago) {
-        this.vida_arquero = vida_arquero;
-        this.ataque_arquero = ataque_arquero;
-        this.defensa_arquero = defensa_arquero;
+    public Batalla(int vida_guerrero, int ataque_guerrero, int defensa_guerrero) {
+        this.vida_guerrero = 80;
+        this.ataque_guerrero = 35;
+        this.defensa_guerrero = 25;
+        this.vida_mago = 65;
+        this.ataque_mago = 55;
+        this.defensa_mago = 20;
+        this.vida_arquero = 70;
+        this.ataque_arquero = 45;
+        this.defensa_arquero = 25;
+    }
+
+    public int getVida_guerrero() {
+        return vida_guerrero;
+    }
+
+    public void setVida_guerrero(int vida_guerrero) {
         this.vida_guerrero = vida_guerrero;
+    }
+
+    public int getAtaque_guerrero() {
+        return ataque_guerrero;
+    }
+
+    public void setAtaque_guerrero(int ataque_guerrero) {
         this.ataque_guerrero = ataque_guerrero;
+    }
+
+    public int getDefensa_guerrero() {
+        return defensa_guerrero;
+    }
+
+    public void setDefensa_guerrero(int defensa_guerrero) {
         this.defensa_guerrero = defensa_guerrero;
+    }
+
+    public int getVida_mago() {
+        return vida_mago;
+    }
+
+    public void setVida_mago(int vida_mago) {
         this.vida_mago = vida_mago;
+    }
+
+    public int getAtaque_mago() {
+        return ataque_mago;
+    }
+
+    public void setAtaque_mago(int ataque_mago) {
         this.ataque_mago = ataque_mago;
+    }
+
+    public int getDefensa_mago() {
+        return defensa_mago;
+    }
+
+    public void setDefensa_mago(int defensa_mago) {
         this.defensa_mago = defensa_mago;
+    }
+
+    public int getVida_arquero() {
+        return vida_arquero;
+    }
+
+    public void setVida_arquero(int vida_arquero) {
+        this.vida_arquero = vida_arquero;
+    }
+
+    public int getAtaque_arquero() {
+        return ataque_arquero;
+    }
+
+    public void setAtaque_arquero(int ataque_arquero) {
+        this.ataque_arquero = ataque_arquero;
+    }
+
+    public int getDefensa_arquero() {
+        return defensa_arquero;
+    }
+
+    public void setDefensa_arquero(int defensa_arquero) {
+        this.defensa_arquero = defensa_arquero;
     }
 }
